3ae1cf1ed875ad41110d09a7d357a89947eaba1d
unknown <justinmoranre@gmail.com>
2023-05-02 21:56:52 -0400

    readme and last changes

--- server.js
+++ server.js
@@ -3,28 +3,24 @@ const fileUpload = require('express-fileupload');
 
 const app = express();
 
-var port = process.env.PORT || 8080;
-
 app.use(fileUpload());
 
-//upload endpoint
-
+// Upload Endpoint
 app.post('/upload', (req, res) => {
-    if(req.files === null) {
-        return res.status(400).json({msg: 'No file uploaded' });
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
     }
 
-    const file = req.files.file;
-
-    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-        if(err) {
-            console.error(err);
-            return res.status(500).send(err);
-        }
-
-        res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
-    });
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
 });
 
-
-app.listen(port, () => console.log('Server Started...'));
+app.listen(5000, () => console.log('Server Started...'));
