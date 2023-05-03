6c37c1815486898102353fe131676370656a29dc
unknown <justinmoranre@gmail.com>
2023-05-02 22:49:42 -0400

    heroku

---
+++ client/build/precache-manifest.b7ca4cce0a6b5f517e1f20645dbfd11d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3129e56058514427c29a",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "3129e56058514427c29a",
+    "url": "/static/js/main.24577513.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "27c43ddf2cf90881e399",
+    "url": "/static/js/2.c59b9399.chunk.js"
+  },
+  {
+    "revision": "48237d89c418c9ad4a32af086bdbecc4",
+    "url": "/index.html"
+  }
+];
